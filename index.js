1295e9c3a7f1f29c265f1adcbd7e99353b744c6d
Camila Santos Sousa <161517829+camila-santino@users.noreply.github.com>
2025-12-08 19:26:17 -0300

    index 1

---
+++ index.js
@@ -0,0 +1,13 @@
+const nome = "Nonsense";
+let qtdXp = 1;
+
+switch (qtdXp) {
+    case qtdXp <= 1000:
+    case qtdXp >= 1:
+        console.log("O Herói " + nome + " está no nível Ferro");
+        break;
+        const nome = "Nonsense"      
+    case qtdXp <= 1000:
+    console.log("O Herói " + nome + " está no nível zerado");
+    break;
+}
