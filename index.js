51a72cd4b1942026f9306787d833a16b9aa7414a
Camila Santos Sousa <161517829+camila-santino@users.noreply.github.com>
2025-12-08 19:44:49 -0300

    index2

---
+++ index.js
@@ -0,0 +1,28 @@
+const nome = "Nonsense";
+let qtdXp = 1;
+
+switch (qtdXp) {
+    case qtdXp <= 1000:
+    case qtdXp >= 1:
+        console.log("O Herói " + nome + " está no nível Ferro");
+    break;
+    case qtdXp <= 1000:
+    case qtdXp >= 1:
+        console.log("O Herói " + nome + " está no nível Ferro");
+    break;
+    case qtdXp <= 1000:
+    case qtdXp >= 1:
+        console.log("O Herói " + nome + " está no nível Ferro");
+    break;
+    case qtdXp <= 1000:
+    case qtdXp >= 1:
+        console.log("O Herói " + nome + " está no nível Ferro");
+    break;
+    case qtdXp <= 1000:
+    case qtdXp >= 1:
+        console.log("O Herói " + nome + " está no nível Ferro");
+    break;
+    case qtdXp <= 1000:3
+    case qtdXp >= 1:
+        console.log("O Herói " + nome + " está no nível Ferro");
+    break;
