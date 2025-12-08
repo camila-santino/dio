61e390410b319bd1596f3efb888b66e5376d4384
Camila Santos Sousa <161517829+camila-santino@users.noreply.github.com>
2025-12-08 19:48:49 -0300

    index3

--- index.js
+++ index.js
@@ -10,4 +10,31 @@ switch (qtdXp) {
     case qtdXp <= 1000:
     console.log("O Herói " + nome + " está no nível zerado");
     break;
-}
+    const nome = "Nonsense";
+    let qtdXp = 1;
+    
+    switch (qtdXp) {
+        case (qtdXp <= 1000):
+        case (qtdXp >= 1):
+            console.log("O Herói " + nome + " está no nível Ferro");
+        break;
+        case qtdXp <= 1000:
+        case qtdXp >= 1:
+            console.log("O Herói " + nome + " está no nível Ferro");
+        break;
+        case qtdXp <= 1000:
+        case qtdXp >= 1:
+            console.log("O Herói " + nome + " está no nível Ferro");
+        break;
+        case qtdXp <= 1000:
+        case qtdXp >= 1:
+            console.log("O Herói " + nome + " está no nível Ferro");
+        break;
+        case qtdXp <= 1000:
+        case qtdXp >= 1:
+            console.log("O Herói " + nome + " está no nível Ferro");
+        break;
+        case default:
+           console.log("O Herói não iniciou a quest.");
+        break;
+    }
