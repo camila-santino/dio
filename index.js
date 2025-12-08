7807d2a42e07283a58286e978fc01c13d856c3ff
Camila Santos Sousa <161517829+camila-santino@users.noreply.github.com>
2025-12-08 23:32:41 +0000

    index depois de entender

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
 const nome = "Nonsense";
 let qtdXp = 1;
-
+ 
 switch (qtdXp) {
-    case qtdXp <= 1000:
-    case qtdXp >= 1:
-        console.log("O Herói " + nome + " está no nível Ferro");
-        break;
-        const nome = "Nonsense"      
-    case qtdXp <= 1000:
-    console.log("O Herói " + nome + " está no nível zerado");
-    break;
-    const nome = "Nonsense";
-    let qtdXp = 1;
-    
-    switch (qtdXp) {
-        case (qtdXp <= 1000):
-        case (qtdXp >= 1):
-            console.log("O Herói " + nome + " está no nível Ferro");
-        break;
-        case qtdXp <= 1000:
-        case qtdXp >= 1:
-            console.log("O Herói " + nome + " está no nível Ferro");
-        break;
-        case qtdXp <= 1000:
-        case qtdXp >= 1:
-            console.log("O Herói " + nome + " está no nível Ferro");
-        break;
-        case qtdXp <= 1000:
-        case qtdXp >= 1:
-            console.log("O Herói " + nome + " está no nível Ferro");
-        break;
-        case qtdXp <= 1000:
-        case qtdXp >= 1:
-            console.log("O Herói " + nome + " está no nível Ferro");
-        break;
-        case default:
-           console.log("O Herói não iniciou a quest.");
-        break;
-    }
+    case (qtdXp <= 1000):
+    case (qtdXp >= 1):
+      console.log("O Herói " + nome + " está no nível Ferro")
+      break;
+    case (qtdXp <= 2001 && qtdXp <= 3000):
+      console.log("O Herói " + nome + " está no nível Prata");
+      break;    
+    case (qtdXp <= 3001 && qtdXp <= 4000):
+      console.log("O Herói " + nome + " está no nível Ouro");
+      break;    
+    case (qtdXp <= 4001 && qtdXp <= 5000):
+      console.log("O Herói " + nome + " está no nível Platina");
+      break;
+    default:
+        console.log("Seu Herói não iniciou a jornada.");
+}   
